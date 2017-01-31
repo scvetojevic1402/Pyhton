9b362e37b998daee12a4105ee2042ab3859e4436
Sreten <scvetojevic1402@gmail.com>
2017-01-31 04:52:49 -0500

    Update fast_set_search.py

--- fast_set_search.py
+++ fast_set_search.py
@@ -30,3 +30,36 @@ def main():
     binary_search(l,234234)
 
 if __name__=="__main__": main()
+    
+
+#Python code line by line profiling:
+#sudo easy_install line_profiler
+#for memory usage use:
+#sudo pip install memory_profiler
+#sudo pip install psutil
+#line profiler:
+#@profile
+#def main():...
+#Usage: kernprof -l -v script.py
+
+#For searches always use sets!!!
+#Much faster than bisect, numpy.searchsorted or a custom defined binary search function!!!
+#A python dictionary is as fast as a set but takes longer to instantiate (let's say all the values are simply 1 d={x:1 for x in some_list}) and takes up more memory
+
+#kernprof -l -v fast_set_search.py
+#Function: main at line 19
+
+#Line #      Hits         Time  Per Hit   % Time  Line Contents
+#==============================================================
+#    19                                           @profile
+#    20                                           def main():
+#    21         1       144922 144922.0      4.7      c=np.random.randint(low=0,high=10000000,size=10000000)
+#    22         1       332271 332271.0     10.7      l=list(c)
+#    23         1      1390709 1390709.0     44.8      s=set(l)
+#    24         1            3      3.0      0.0      if 234234 in s:
+#    25                                                   print 1
+#    26         1       611344 611344.0     19.7      if 234234 in l:
+#    27                                                   print 1
+#    28         1           15     15.0      0.0      bisect.bisect(l,234234)
+#    29         1       622612 622612.0     20.1      np.searchsorted(l,234234)
+#    30         1           41     41.0      0.0      binary_search(l,234234)
